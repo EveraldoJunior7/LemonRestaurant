1c4c3b57364229ef7c7f53537ee2ba2b0c3c6531
Everaldo Junior Borges Garcia De Padua <a57328@alunos.ipb.pt>
2023-04-18 23:15:31 +0100

    LoginScreen

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import MenuItems from './components/MenuItems';
+import LoginScreen from './LoginScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItems />
+        <LoginScreen />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
